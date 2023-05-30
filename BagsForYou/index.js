496a5c4ffaa6e181efd2d756a07027d2a3b82d51
Adrian <42110361+xSteins@users.noreply.github.com>
2023-05-30 22:43:27 +0700

    Initial expressJS app templating

---
+++ BagsForYou/index.js
@@ -0,0 +1,38 @@
+import 'boxicons';
+import express from 'express';
+import mysql from 'mysql';
+import path from 'path';
+import bodyParser from 'body-parser';
+
+import crypto from 'crypto'
+import session from 'express-session';
+
+const app = express();
+const port = 8080;
+const publicPath = path.resolve('public')
+
+app.use(express.static(publicPath));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MySQL Connection
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'MIBD-BagsForYou',
+    // Undecided, harus dibuat lagi nanti
+});
+// Middleware connection 
+app.use(
+    session({
+        secret: 'must be filled later...',
+        resave: false,
+        saveUninitialized: true
+    })
+);
+
+app.listen(port, () => {
+    console.log('App started');
+    console.log(`Server running on http://localhost:${port}`);
+})
