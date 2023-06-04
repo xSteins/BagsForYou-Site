a610d4735d04a003413ec90ce0c07fad2c104623
Adrian <42110361+xSteins@users.noreply.github.com>
2023-06-04 16:46:47 +0700

    Updated code to display login / signup modal

--- BagsForYou/static-path/js/script.js
+++ BagsForYou/static-path/js/script.js
@@ -1,9 +1,18 @@
-// Redirect for navbar button
+const loginMenu = document.querySelector('#login');
+const signUpMenu = document.querySelector('#signup');
 
+// Display the login / signup modal
 document.getElementById('SignUpButton').addEventListener('click', function () {
-    window.location.href = '/signup';
+    signUpMenu.classList.remove('hidden');
+});
+document.getElementById('SignInButton').addEventListener('click', function () {
+    loginMenu.classList.remove('hidden');
 });
 
-document.getElementById('SignInButton').addEventListener('click', function () {
-    window.location.href = '/login';
-});
+// Close the window button
+const windowCloseButton = document.querySelector('#close-window-link');
+
+windowCloseButton.addEventListener('click', () => {
+    signUpMenu.classList.add('hidden');
+    loginMenu.classList.add('hidden');
+})
